36a99ce2374e9fffbbcd336a3e4642a0d824c1b9
concat347 <152508873+concat347@users.noreply.github.com>
2026-01-25 09:39:10 -0600

    Enhance app with status endpoint

--- flask-app/app.py
+++ flask-app/app.py
@@ -6,5 +6,14 @@ def hello_world():
 
     return 'Hello MSOE'
 
+# This is the "Enhancement"
+@app.route('/status')
+def get_status():
+    return jsonify({
+        "status": "online",
+        "message": "Microservice enhanced successfully!",
+        "version": "2.0"
+    })
+
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=8080)
